d422222f6ffd73a0fbe78bcfa3d888819b92c316
Jared <1337055758@qq.com>
2025-02-26 18:38:00 +0800

    add new files to my repo.

---
+++ t-小菜编程日记/deepseek.py
@@ -0,0 +1,24 @@
+# Please install OpenAI SDK first: `pip3 install openai`
+
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-489a6ce769fe41a9811440ce29522426", base_url="https://api.deepseek.com")
+# Round 1
+messages = [{"role": "user", "content": "What's the highest mountain in the world?"}]
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=messages
+)
+
+messages.append(response.choices[0].message)
+print(f"Messages Round 1: {messages}")
+
+# Round 2
+messages.append({"role": "user", "content": "What is the second?"})
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=messages
+)
+
+messages.append(response.choices[0].message)
+print(f"Messages Round 2: {messages}")
